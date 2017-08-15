ca646554e65145401a4010a35d869f5e228b872c
liuxuanshuo <lxuanshuo@gmail.com>
2017-08-15 16:36:43 +0800

    提交
    提交完整代码

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '8.0'
+target "LXS_cocopod" do
+pod 'FMDB', '~> 2.6.2'
+end
---
+++ Pods/Target Support Files/Pods-LXS_cocopod/Pods-LXS_cocopod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LXS_cocopod : NSObject
+@end
+@implementation PodsDummy_Pods_LXS_cocopod
+@end
